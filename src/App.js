648c9898f5d4cacb5b1ebce3992b3f5dfaec8039
civille <77638935+civille@users.noreply.github.com>
2021-09-13 19:09:54 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Civille</h1>
+      <h2>Everything starts here!</h2>
+    </div>
+  );
+}
